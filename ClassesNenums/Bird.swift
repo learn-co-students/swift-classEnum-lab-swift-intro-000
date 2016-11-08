83d5886ef7cd0fdff3fa5f636e6ee3c23a432e51
Alex .P <persson.ac@gmail.com>
2016-11-08 12:56:42 -0500

    All tests done in one turn.

--- ClassesNenums/Bird.swift
+++ ClassesNenums/Bird.swift
@@ -9,8 +9,55 @@
 import Foundation
 
 
+enum Speed: Int {
+    case slow, medium, fast
+    
+    func isFaster(thanSpeed: Speed) -> Bool {
+        if self.rawValue >= thanSpeed.rawValue {
+            return true
+        } else {
+            return false
+        }
+    }
+}
 
+enum Diet {
+    case meatEater, vegetarian
+}
 
+enum Sex {
+    case male, female
+}
 
+class Trex {
+    
+    var speed: Speed = .fast
+    var diet: Diet = .meatEater
+    var name: String
+    var sex: Sex
+    var isAlive: Bool = true
+    
+    init(name: String, sex: Sex) {
+        self.name = name
+        self.sex = sex
+    }
+    
+    func speak() -> String {
+        return "ROAAAWWWWRRRRR!!!!!!"
+    }
+    
+    func isFaster(thanTrex trex:Trex) -> Bool {
+        return self.speed.isFaster(thanSpeed: trex.speed)
+    }
+    
+    func eat(otherTrex trex:Trex) -> Bool {
+        if self.isFaster(thanTrex: trex) {
+            trex.isAlive = false
+            return true
+        } else {
+            return false
+        }
+    }
+}
 
 
