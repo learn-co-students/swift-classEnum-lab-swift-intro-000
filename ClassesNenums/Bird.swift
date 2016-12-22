9af1208a03dc7b02c07fc36d884c8bdacbfa3f39
Subcipher <krishna@javaworkgroup.com>
2016-12-22 08:16:52 -0500

    completed Trex enum lab

--- ClassesNenums/Bird.swift
+++ ClassesNenums/Bird.swift
@@ -9,6 +9,67 @@
 import Foundation
 
 
+enum Speed: Int{
+    case slow = 1
+    case medium
+    case fast
+    
+    func isFaster(thanSpeed:Speed)->Bool{
+        
+        return self.rawValue >= thanSpeed.rawValue
+    }
+}
+
+
+enum Diet{
+    case meatEater
+    case vegetarian
+    
+}
+
+enum Sex{
+    case male
+    case female
+    
+}
+
+
+class Trex{
+    
+    var speed:Speed = Speed.fast
+    let diet:Diet = Diet.meatEater
+    let name:String
+    let sex:Sex
+    
+    var isAlive:Bool = true
+    
+    init(name:String,sex:Sex){
+        self.name = name
+        self.sex = sex
+    }
+    
+    func speak()->String{
+        return "ROAAAWWWWRRRRR!!!!!!"
+    }
+    func isFaster(thanTrex:Trex)->Bool{
+        return self.speed.rawValue > thanTrex.speed.rawValue
+    }
+    
+    func eat(otherTrex:Trex){
+        otherTrex.isAlive = false
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
