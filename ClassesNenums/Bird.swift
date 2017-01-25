ea89218489f4aa8803440be024684e30a01a9969
Ryan Hennings <r.hennings11@gmail.com>
2017-01-25 00:23:46 -0800

    Enums dinosaurs lab

--- ClassesNenums/Bird.swift
+++ ClassesNenums/Bird.swift
@@ -8,6 +8,57 @@
 
 import Foundation
 
+enum Speed: Int {
+    case slow = 1, medium, fast
+    
+    func isFaster(thanSpeed: Speed) -> Bool {
+        if thanSpeed.rawValue <= self.rawValue {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+enum Diet {
+    case meatEater
+    case vegetarian
+}
+
+enum Sex {
+    case male
+    case female
+}
+
+class Trex {
+    var speed: Speed
+    var diet: Diet
+    var name: String
+    var sex: Sex
+    var isAlive: Bool
+    
+    init(name: String, sex: Sex) {
+        self.name = name
+        self.sex = sex
+        speed = Speed.fast
+        diet = Diet.meatEater
+        isAlive = true
+    }
+    
+    func speak() -> String {
+        return "ROAAAWWWWRRRRR!!!!!!"
+    }
+    
+    func isFaster(thanTrex: Trex) -> Bool {
+        return self.speed.isFaster(thanSpeed: thanTrex.speed)
+    }
+    
+    func eat(otherTrex: Trex) {
+        if self.isFaster(thanTrex: otherTrex) {
+            otherTrex.isAlive = false
+        }
+    }
+}
 
 
 
