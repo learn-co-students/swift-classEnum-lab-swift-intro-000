e0d4f80b1003a58070c3e1d9e783a215780ae382
othmansafarini <othman_safarini@live.co.uk>
2017-02-16 12:24:41 +0400

    OK

--- ClassesNenums/Bird.swift
+++ ClassesNenums/Bird.swift
@@ -8,6 +8,29 @@
 
 import Foundation
 
+enum Speed: Int {
+    
+    static let speeds: [Speed] = [.slow, .medium, .fast]
+    
+    case slow = 0
+    case medium = 1
+    case fast = 2
+    
+    func isFaster(thanSpeed: Speed) -> Bool {
+        if thanSpeed == Speed.slow < Speed.medium {
+            return true
+        }
+        return false
+    }
+}
+
+enum Diet {
+    case meatEater, vegetarian
+}
+
+enum sex {
+    case male, female
+}
 
 
 
