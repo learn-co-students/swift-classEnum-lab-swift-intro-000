2119fb44234195af1870c527ed543ef080ee94d1
Brendt Bly <brendtbly@Brendts-MacBook-Pro.local>
2017-03-17 08:37:09 -0500

    Completed problems

--- ClassesNenums/Bird.swift
+++ ClassesNenums/Bird.swift
@@ -9,8 +9,47 @@
 import Foundation
 
 
+enum Speed: Int {
+    case slow, medium, fast
+    
+    func isFaster(thanSpeed: Speed) -> Bool {
+        guard self.rawValue > thanSpeed.rawValue else {return false}
+            return true
+    }
 
+}
 
+enum Diet {
+    case meatEater, vegetarian
+}
 
+enum Sex {
+    case male, female
+}
 
-
+class Trex {
+    let diet: Diet = Diet.meatEater
+    let name: String
+    let sex: Sex
+    var isAlive: Bool = true
+    var speed: Speed = .fast
+    
+    init(name: String, sex: Sex) {
+        self.name = name
+        self.sex = sex
+    }
+    
+    func speak() -> String {
+        return "ROAAAWWWWRRRRR!!!!!!"
+    }
+    
+    func isFaster(thanTrex: Trex) -> Bool {
+        return self.speed.rawValue > thanTrex.speed.rawValue
+    }
+    
+    func eat(otherTrex: Trex) {
+        if isFaster(thanTrex: otherTrex) {
+            otherTrex.isAlive = false
+        }
+    }
+}
