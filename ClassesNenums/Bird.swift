253ac7d977e501e452bbce4a8d31c3b491378ae3
NateDukatz <natedukatz@gmail.com>
2017-05-16 00:12:51 -0600

    class enum lab complete

--- ClassesNenums/Bird.swift
+++ ClassesNenums/Bird.swift
@@ -9,6 +9,62 @@
 import Foundation
 
 
+enum Speed : Int {
+    case slow
+    case medium
+    case fast
+    
+    func isFaster(thanSpeed: Speed) -> Bool {
+        if self.rawValue >= thanSpeed.rawValue {
+            return true
+        }
+        return false
+    }
+}
+
+
+enum Diet {
+    case meatEater
+    case vegetarian
+}
+
+enum Sex {
+    case male
+    case female
+}
+
+class Trex {
+    var speed : Speed = Speed.fast
+    var diet : Diet = Diet.meatEater
+    var name : String
+    var sex : Sex
+    var isAlive : Bool = true
+    
+    
+    init(name: String, sex: Sex){
+        self.name = name
+        self.sex = sex
+    }
+    
+    func speak() -> String {
+        return "ROAAAWWWWRRRRR!!!!!!"
+    }
+    
+    func isFaster(thanTrex: Trex) -> Bool {
+        if self.speed.isFaster(thanSpeed: thanTrex.speed){
+            return true
+        }
+        return false
+    }
+    
+    func eat(otherTrex: Trex){
+        if self.speed.isFaster(thanSpeed: otherTrex.speed){
+            otherTrex.isAlive = false
+        }
+    }
+}
+
+
 
 
 
