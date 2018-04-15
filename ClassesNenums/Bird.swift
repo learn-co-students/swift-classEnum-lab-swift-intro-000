46c46d80264edf8c3939fdd811976de1db4602e7
MlleDolce <andreakchristie@gmail.com>
2018-04-14 20:09:57 -0400

    Completed Enums Dinosaurs Lab.  All tests pass.

--- ClassesNenums/Bird.swift
+++ ClassesNenums/Bird.swift
@@ -8,7 +8,50 @@
 
 import Foundation
 
+enum Speed: Int {
+    case slow = 1
+    case medium = 2
+    case fast = 3
+    
+    func isFaster(thanSpeed: Speed) -> Bool {
+        return self.rawValue >= thanSpeed.rawValue ? true : false
+    }
+}
 
+enum Diet {
+    case meatEater
+    case vegetarian
+}
+
+enum Sex {
+    case male
+    case female
+}
+
+class Trex {
+    var speed: Speed = .fast
+    var diet: Diet = .meatEater
+    var name: String
+    var sex: Sex
+    var isAlive: Bool = true
+    
+    init(name: String, sex: Sex) {
+        self.name = name
+        self.sex = sex
+    }
+    
+    func speak() -> String {
+        return "ROAAAWWWWRRRRR!!!!!!"
+    }
+    
+    func isFaster(thanTrex: Trex) -> Bool {
+        return self.speed.rawValue > thanTrex.speed.rawValue ? true : false
+    }
+    
+    func eat(otherTrex: Trex) {
+        otherTrex.isAlive = false
+    }
+}
 
 
 
