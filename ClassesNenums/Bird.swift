5a444ed77f6b5f3b89cb8d90f798f2c74927d7a2
mkes99 <mike@formativewebsolutions.com>
2018-09-06 22:06:55 -0700

    Adding lab

--- ClassesNenums/Bird.swift
+++ ClassesNenums/Bird.swift
@@ -8,9 +8,47 @@
 
 import Foundation
 
+enum Speed : Int {
+    case slow, medium, fast
+    
+    func isFaster(thanSpeed speed: Speed) -> Bool {
+        return self.rawValue > speed.rawValue
+    }
+}
 
+enum Diet {
+    case meatEater, vegetarian
+}
 
+enum Sex {
+    case male, female
+}
 
-
-
-
+class Trex {
+    var speed = Speed.fast
+    let diet = Diet.meatEater
+    
+    let name: String
+    let sex: Sex
+    var isAlive = true
+    
+    
+    init(name : String, sex: Sex) {
+        self.name = name
+        self.sex = sex
+    }
+    
+    func speak() -> String {
+        return "ROAAAWWWWRRRRR!!!!!!"
+    }
+    
+    func isFaster(thanTrex trex: Trex) -> Bool {
+        return self.speed.rawValue > trex.speed.rawValue
+    }
+    
+    func eat(otherTrex trex: Trex) {
+        if self.speed.rawValue > trex.speed.rawValue{
+            trex.isAlive = false
+        }
+    }
+}
