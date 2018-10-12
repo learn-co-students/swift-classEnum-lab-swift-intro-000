bcd83f8bc660dc58de21558d44415517e1248635
bodeacorina <corina.bodea@yahoo.com>
2018-10-12 17:01:53 +0300

    Add classEnum solution

--- ClassesNenums/Bird.swift
+++ ClassesNenums/Bird.swift
@@ -8,6 +8,24 @@
 
 import Foundation
 
+enum Speed: Int {
+    case slow, medium, fast
+
+
+    func isFaster(thanSpeed: Speed) -> Bool {
+        return self.rawValue >= thanSpeed.rawValue
+    }
+}
+
+enum Diet {
+    case meatEater
+    case vegetarian
+}
+
+enum Sex {
+    case male
+    case female
+}
 
 
 
